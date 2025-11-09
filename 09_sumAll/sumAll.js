a5e8c52c00970edde773cce0ae4bfa6138234f5d
Michael Morris <michaelmorris121@yahoo.com>
2025-11-09 11:31:51 -0600

    create function for sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) {return "ERROR";}
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {return "ERROR";}
+    let total = 0
+    let start = Math.min(min, max);
+    let end = Math.max(min, max);
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// take 2 integers 
+// return sum of every integer between and including them
