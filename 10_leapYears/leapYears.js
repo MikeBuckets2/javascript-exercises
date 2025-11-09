040825ca9e2c374fc11e365b470fa3180ea08e33
Michael Morris <michaelmorris121@yahoo.com>
2025-11-09 11:53:58 -0600

    create function to find leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//write a function that tells if a year is a leap year
+//if divisible by 4 return true
+//if divisible by 100 return false
+//if divisible by 400 return true
+//else return false 
