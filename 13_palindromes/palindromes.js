287371085fdb362ebb1e68968614809a1f937ebe
Michael Morris <michaelmorris121@yahoo.com>
2025-11-21 12:14:55 -0600

    create function for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   const chr = "abcdefghijklmnopqrstuvwxyz1234567890";
+    const oldStr = str
+        .toLowerCase()
+        .split("")
+        .filter((character) => chr.includes(character))
+        .join("");
+    const reverseStr = oldStr.split("").reverse().join("");
+    return oldStr === reverseStr;
 };
 
 // Do not edit below this line
