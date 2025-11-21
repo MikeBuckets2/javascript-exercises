32e9aa759c0e0561e0f73881c44fe897041f57ac
Michael Morris <michaelmorris121@yahoo.com>
2025-11-21 12:48:33 -0600

    create function for fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let count 
+    if (typeof num !== "number") {
+        count = parseInt(num);
+    } else {
+        count = num;
+    };
+    if (count < 0) {return "OOPS";};
+    if (count == 0) {return 0;};
 
+    let current = 1;
+    let previous = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let newNum = current + previous;
+        previous = current;
+        current = newNum;
+    };
+
+    return current;
 };
 
 // Do not edit below this line
